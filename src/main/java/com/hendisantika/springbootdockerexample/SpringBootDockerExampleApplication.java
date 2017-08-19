550c4cc7bec7bef1e46b2213ce55eeb47655bb2d
Hendi Santika <hendisantika@gmail.com>
2017-08-20 05:21:01 +0700

    #5 - Added new properties

--- src/main/java/com/hendisantika/springbootdockerexample/SpringBootDockerExampleApplication.java
+++ src/main/java/com/hendisantika/springbootdockerexample/SpringBootDockerExampleApplication.java
@@ -2,9 +2,29 @@ package com.hendisantika.springbootdockerexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerExampleApplication {
+
+	static final String PATH_HOME = "/";
+	static final String PATH_DERP = "/derp";
+	static final String PATH_SYSTEM_PROPERTIES = "/_/properties";
+	static final String PATH_ENVIRONMENT_VARIABLES = "/_/environment";
+
+	@Value("${io.dweomer.example.derp}")
+	private String derp;
+
+	@RequestMapping(produces = "text/plain", path = PATH_HOME)
+	public String home() {
+		return "Hello Docker World";
+	}
+
+	@RequestMapping(produces = "text/plain", path = PATH_DERP)
+	public String derp() {
+		return derp;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerExampleApplication.class, args);
