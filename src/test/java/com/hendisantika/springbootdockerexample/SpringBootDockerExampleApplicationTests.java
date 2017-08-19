4b0baf3b115b9ff32f8c0fe1aae1bd3155822ba1
Hendi Santika <hendisantika@gmail.com>
2017-08-20 05:34:06 +0700

    #9 - Added test case

--- src/test/java/com/hendisantika/springbootdockerexample/SpringBootDockerExampleApplicationTests.java
+++ src/test/java/com/hendisantika/springbootdockerexample/SpringBootDockerExampleApplicationTests.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootDockerExampleApplicationTests {
@@ -13,4 +16,18 @@ public class SpringBootDockerExampleApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testHome() {
+		assertEquals("Hello Docker World", new SpringBootDockerExampleApplication().home());
+	}
+
+	@Test
+	public void testEnvironmentVariables() {
+		assertNotNull(new SpringBootDockerExampleApplication().environmentVariables());
+	}
+
+	@Test
+	public void testSystemProperties() {
+		assertNotNull(new SpringBootDockerExampleApplication().systemProperties());
+	}
 }
